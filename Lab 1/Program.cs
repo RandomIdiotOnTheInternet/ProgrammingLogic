f745a4163628395a87676841612627907a423112
Gregory Blazek <gregory.r.blazek@gmail.com>
2025-03-17 16:18:11 -0400

    Lab 1 Completion

--- Lab 1/Program.cs
+++ Lab 1/Program.cs
@@ -1,7 +1,31 @@
 using System;
-class Program
-{
- static void Main() {
-    Console.WriteLine("Hello, world!");
- }
-}
+
+   class Program
+   {
+   static void Main() {
+         // Step 1
+         Console.WriteLine("Hello, world!");
+         Console.WriteLine("Gregory Blazek");
+         Console.WriteLine("IT-1050-0-Programming Logic");
+
+         // Step 2
+         /*
+         Name: Gregory Blazek
+         Title: IT-1050 - Lab 1
+         */
+
+         // Step 3
+         int favoriteNumber = 3;
+         string favoriteProgrammingLanguage = "Python";
+         double programsWritten = 6.0;
+         bool hasProgrammingExperience = true;
+         Console.WriteLine("My favorite number is " + favoriteNumber);
+         Console.WriteLine("My favorite programming language is " + favoriteProgrammingLanguage);
+         Console.WriteLine("I've written " + programsWritten + " programs");
+         Console.WriteLine(hasProgrammingExperience + ", I do have coding experience");
+
+         // Step 4
+         const string schoolName = "Tri-C";
+         Console.WriteLine("I go to " + schoolName);
+      }
+   }
