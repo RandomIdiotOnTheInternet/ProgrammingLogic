1a69ee89212622a3b7ab22c791e0c0d2921b7072
RandomIdiotOnTheInternet <139891696+RandomIdiotOnTheInternet@users.noreply.github.com>
2025-03-24 18:50:58 +0000

    Change from VS code to Codespaces

---
+++ Lab1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+   class Program
+   {
+   static void Main(string[] args) {
+         // Step 1
+         Console.WriteLine("Gregory Blazek");
+         Console.WriteLine("IT-1050-0-Programming Logic");
+
+         // Step 2
+         /*
+         Name: Gregory Blazek
+         Title: IT-1050 - Lab 1
+         */
+
+         // Step 3
+         int favoriteNumber = 3;
+         string favoriteProgrammingLanguage = "Python";
+         double programsWritten = 6.0;
+         bool hasProgrammingExperience = true;
+         Console.WriteLine("My favorite number is " + favoriteNumber);
+         Console.WriteLine("My favorite programming language is " + favoriteProgrammingLanguage);
+         Console.WriteLine("I've written " + programsWritten + " programs");
+         Console.WriteLine(hasProgrammingExperience + ", I do have coding experience");
+
+         // Step 4
+         const string schoolName = "Tri-C";
+         Console.WriteLine("I go to " + schoolName);
+      }
+   }
