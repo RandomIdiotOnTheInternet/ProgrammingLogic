6a6340addb126d9278bab52f2d75144e24cc5c4e
RandomIdiotOnTheInternet <139891696+RandomIdiotOnTheInternet@users.noreply.github.com>
2025-04-01 14:05:54 +0000

    Lab 3 Completion

--- Lab1Codespaces/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1Codespaces/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc3cd5bf2b5b5870ea38874af3c0edbc3d12071e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2205d66b46d28429be1fb6121bf85d2a6b7dfe1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab3/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace Lab3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Part 1
+
+        int a = 10;
+        int b = 5;
+        int c = 15;
+
+        Console.WriteLine(a > b);
+        Console.WriteLine(a < c);
+        Console.WriteLine(a > b && a > c);
+        Console.WriteLine(c < b || c > a);
+
+        // Part 2
+
+        bool isRaining = true;
+        bool haveUmbrella = false;
+        if (isRaining && !haveUmbrella)
+        {
+            Console.WriteLine("Take an umbrella!");
+        } else
+        {
+            Console.WriteLine("You're good to go!");
+        }
+
+
+        // Part 3
+
+        Console.WriteLine("What's your age?");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if (age < 5)
+        {
+            Console.WriteLine("Ticket is free!");
+        } else if (age <= 12)
+        {
+            Console.WriteLine("Child ticket: $5");
+        } else if (age <= 64)
+        {
+            Console.WriteLine("Standard ticket: $10");
+        } else
+        {
+            Console.WriteLine("Senior ticket: $6");
+        }
+
+        // Part 4
+        
+        Console.WriteLine("What day of the week is it? (Please enter 1-7, where 1 is Monday)");
+        int dayOfTheWeek = Convert.ToInt32(Console.ReadLine());
+
+        switch (dayOfTheWeek)
+        {
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Wednesday");
+                break;
+            case 4:
+                Console.WriteLine("Thursday");
+                break;
+            case 5:
+                Console.WriteLine("Friday");
+                break;
+            case 6:
+                Console.WriteLine("Saturday");
+                break;
+            case 7:
+                Console.WriteLine("Sunday");
+                break;
+            default:
+                Console.WriteLine("Invalid day!");
+                break;
+        }
+    }
+}
