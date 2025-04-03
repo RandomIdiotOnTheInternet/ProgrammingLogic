d4c743b7d42d79fc01349a26a3d6561bdb9f0394
RandomIdiotOnTheInternet <139891696+RandomIdiotOnTheInternet@users.noreply.github.com>
2025-04-03 19:24:43 +0000

    Created templates for future labs

--- Lab1Codespaces/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1Codespaces/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2205d66b46d28429be1fb6121bf85d2a6b7dfe1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a6340addb126d9278bab52f2d75144e24cc5c4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
