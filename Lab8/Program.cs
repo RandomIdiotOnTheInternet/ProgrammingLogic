8982768fde70747fac9874cb205ec7920b969fdb
RandomIdiotOnTheInternet <139891696+RandomIdiotOnTheInternet@users.noreply.github.com>
2025-05-06 13:01:19 +0000

    Lab 8 Completion

--- Lab8/Program.cs
+++ Lab8/Program.cs
@@ -1,9 +1,48 @@
 ﻿namespace Lab8;
 
+using System;
+
+// Step 1
+
+class Animal
+{
+  public virtual void animalSound()
+  {
+    Console.WriteLine("The animal makes a sound");
+  }
+}
+
+// Step 2
+
+class Cat : Animal
+{
+  public override void animalSound()
+  {
+    Console.WriteLine("The cat says: meow");
+  }
+}
+
+class Dog : Animal
+{
+  public override void animalSound()
+  {
+    Console.WriteLine("The dog says: bow wow");
+  }
+}
+
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+  static void Main(string[] args)
+  {
+    
+    // Step 3
+
+    Animal myAnimal = new Animal();
+    Animal myCat = new Cat();
+    Animal myDog = new Dog();
+    myAnimal.animalSound();
+    myCat.animalSound();
+    myDog.animalSound();
+    
+  }
 }
